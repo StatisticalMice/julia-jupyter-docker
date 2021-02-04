daf29db984fdd2557e7d28d20e0d39bfd0c11817
Ari <ari.huttunen@me.com>
2021-02-04 05:56:06 +0200

    get_info.sh

---
+++ get_info.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+image=statisticalmice/julia-jupyter:1.6-buster
+echo
+echo $image
+echo 
+docker run -it --rm $image bash -c "julia -version; python --version; jupyter --version | grep lab; conda --version; echo Debian:; cat /etc/debian_version"
+echo
+
+image=statisticalmice/julia-jupyter:1.5-buster
+echo $image
+echo 
+docker run -it --rm $image bash -c "julia -version; python --version; jupyter --version | grep lab; conda --version; echo Debian:; cat /etc/debian_version"
