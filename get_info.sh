b6d9a460d7922cc27bbb68a90b24b904b7253210
Ari <ari.huttunen@me.com>
2021-03-14 14:24:24 +0200

    tooling

--- get_info.sh
+++ get_info.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
+image=statisticalmice/julia-jupyter:1.6-buster-max
+echo
+echo $image
+echo 
+docker run -it --rm $image bash -c "julia -version; python --version; jupyter --version | grep lab; conda --version; echo Debian:; cat /etc/debian_version; glances --version"
+docker run -it --rm $image bash -c "rr --version"
+echo
+
 image=statisticalmice/julia-jupyter:1.6-buster
 echo
 echo $image
 echo 
-docker run -it --rm $image bash -c "julia -version; python --version; jupyter --version | grep lab; conda --version; echo Debian:; cat /etc/debian_version"
+docker run -it --rm $image bash -c "julia -version; python --version; jupyter --version | grep lab; conda --version; echo Debian:; cat /etc/debian_version; glances --version"
 echo
 
 image=statisticalmice/julia-jupyter:1.5-buster
 echo $image
 echo 
-docker run -it --rm $image bash -c "julia -version; python --version; jupyter --version | grep lab; conda --version; echo Debian:; cat /etc/debian_version"
+docker run -it --rm $image bash -c "julia -version; python --version; jupyter --version | grep lab; conda --version; echo Debian:; cat /etc/debian_version; glances --version"
