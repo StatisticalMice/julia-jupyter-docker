82b72b25325c987696e8039ae75780a23a068466
Ari <ari.huttunen@me.com>
2021-03-19 11:32:25 +0200

    jupyterlab=3.0.10 as 3.0.11 seemed broken

--- push-to-hub.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -Eeuo pipefail
-
-docker push statisticalmice/julia-jupyter:latest
-docker push statisticalmice/julia-jupyter:1.5-buster
-docker push statisticalmice/julia-jupyter:1.6-buster
